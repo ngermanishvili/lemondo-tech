a0b3ce65ad0af7330f843c177089799a1d03a1b2
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-09-28 20:17:51 +0400

    masthead zustand store added 🏪

---
+++ store/masthead.store.ts
@@ -0,0 +1,11 @@
+import { create, SetState } from "zustand";
+
+export const useBasketStore = create<{
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}>((set: SetState<{ count: number }>) => ({
+  count: 0,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+  dec: () => set((state) => ({ count: state.count - 1 })),
+}));
