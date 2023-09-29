b282e64570cdf84f70dc9e8c7023440a24d01c5e
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-09-29 16:45:35 +0400

    use zustand store n modified it globally 🌎

---
+++ components/primarySidebar/components/Search/index.tsx
@@ -0,0 +1,30 @@
+"use client"
+import React, { ChangeEvent } from 'react';
+import styles from '@/styles/components/SearchBar.module.scss';
+
+interface SearchBarProps {
+    searchQuery: string;
+    setSearchQuery: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ searchQuery, setSearchQuery }) => {
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    };
+
+    return (
+        <>
+            <div className={styles.searchQ}>
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={handleInputChange}
+                    placeholder="სახელით ძიება"
+                    className={styles.input}
+                />
+            </div>
+        </>
+    );
+};
+
+export default SearchBar;
