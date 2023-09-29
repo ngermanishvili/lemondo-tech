4525750228d4b82265f7e7064794220f499cfebc
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-09-29 16:43:24 +0400

    sidebar header modified styling

---
+++ components/mobile/sidebarHeader/index.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useState } from 'react';
+import Image from 'next/image';
+import styles from '@/styles/components/sidebarHeader.module.scss'
+import Cancel from '../.././../public/images/cancel.svg'
+
+interface SidebarHeaderProps {
+  onClose: () => void;
+}
+
+export const SidebarHeader: FC<SidebarHeaderProps> = ({ onClose }) => {
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <div className={styles.headerfilt}>
+      <span>ფილტრი</span>
+      <Image
+        src={Cancel}
+        width={22}
+        height={22}
+        alt="CloseButton, დახურვა"
+        className={styles.close}
+        onClick={handleClose}
+      />
+    </div>
+  );
+};
