513dae8b79d32f86484bfde68bb6ce6474f3d7a3
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-09-30 06:05:32 +0400

    🚀

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
 
-import NavBar from '../../../components/navbar/index'
-import Banner from "../../../components/banner/index"
-import SortComponent from "../../../components/sort/index";
-import SideBarHero from "../../../components/primarySidebar/index"
-import styles from '../../page.module.scss';
+import NavBar from '@/components/navbar/index'
+import Banner from '@/components/banner/index'
+import SortComponent from "@/components/sort/index";
+import SideBarHero from "@/components/primarySidebar/index"
+import styles from '../../page.module.scss';  
 
 
 
