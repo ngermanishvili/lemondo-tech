f9011e58c8cf25d1ad1a68324b0a818881e13f78
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-09-30 12:42:04 +0400

    update dependecies n config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-/** @type {import('next').NextConfig} */
 const path = require('path')
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
     reactStrictMode: false,
-
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname),
+        };
+        return config;
+    },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
